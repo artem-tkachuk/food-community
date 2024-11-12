321cacb99dab0410c0cfcd5e09fe5c5f43023fe6
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-11-11 21:08:14 -0800

    Link to about page using Link component that doesn't load a brand new page from the server

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 // This is a server component
 export default function Home() {
     console.log(`Hello from the server!`)
@@ -7,6 +9,7 @@ export default function Home() {
             <img src="/logo.png" alt="A server surrounded by magic sparkles." />
             <h1>Welcome to this NextJS Course!</h1>
             <p>🔥 Let&apos;s get started! 🔥</p>
+            <p><Link href="/about">About us</Link></p>
         </main>
     );
 }
